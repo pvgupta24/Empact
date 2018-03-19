4169bd1235d586473b4b0ae1712273ff2bbc3881
Praveen Gupta <pvgupta24@gmail.com>
2018-03-20 01:17:13 +0530

    Blobs

--- routes/webCamImage.js
+++ routes/webCamImage.js
@@ -3,6 +3,24 @@ var router = express.Router();
 var path = require('path');
 var base64Img = require('base64-img');
 
+// var rawdata = 'data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==';
+// var matches = rawdata.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+// var type = matches[1];
+// var buffer = new Buffer(matches[2], 'base64');
+//
+// blobsrv.createBlockBlobFromText('empactstoragecontainer','profile-pic-123.jpg', buffer, {contentType:type}, function(error, result, response) {
+//     if (error) {
+//         console.log(error);
+//     }else{
+//         console.log(result)
+//     }
+// });
+// var azure = require('azure-storage');
+// var blobsrv = azure.createBlobService(
+//     '<storage_account>',
+//     '<storage_key>'
+// );
+// blobsrv.getBlobToStream('<container>', '<blob>', res, function (error, result) {});
 var i = 0;
 /* Save image to public folder */
 router.post('/', function(req, res, next) {
